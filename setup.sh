e2539b747b47ba6b4244515ff1555b94ca37d79f
Yuki Anzai <yanzm@uphyca.com>
2013-02-15 11:53:30 +0900

    fix error message for grunt

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ fi
 which grunt > /dev/null 2>&1
 if [ $? -ne 0 ] ; then
 	echo "command not found: grunt"
-	echo "please install npm. e.g. npm install -g grunt"
+	echo "please install grunt. e.g. npm install -g grunt"
 	ISERROR=1
 fi
 
