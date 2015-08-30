802d60dac9cc437fb62183d91a49c0e31ab8980c
vvakame <vvakame+dev@gmail.com>
2015-08-30 12:59:38 +0900

    try tsconfig.json with grunt-ts

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,29 +16,15 @@ module.exports = function(grunt) {
 		},
 
 		ts: {
-			options: {
-				compile: true,                 // perform compilation. [true (default) | false]
-				comments: false,               // same as !removeComments. [true | false (default)]
-				target: 'es5',                 // target javascript language. [es3 (default) | es5]
-				module: 'commonjs',            // target javascript module style. [amd (default) | commonjs]
-				noImplicitAny: true,
-				sourceMap: false,              // generate a source map for every output js file. [true (default) | false]
-				sourceRoot: '',                // where to locate TypeScript files. [(default) '' == source ts location]
-				mapRoot: '',                   // where to locate .map.js files. [(default) '' == generated js location.]
-				declaration: false,            // generate a declaration .d.ts file for every output js file. [true | false (default)]
-				experimentalDecorators: true
-			},
-			clientCli: {
-				src: ['<%= opt.client.tsMain %>/cli.ts']
-			},
 			clientMain: {
-				src: ['<%= opt.client.tsMain %>/index.ts'],
 				options: {
-					declaration: true
+					tsconfig: '<%= opt.client.tsMain %>/tsconfig.json'
 				}
 			},
 			clientTest: {
-				src: ['<%= opt.client.tsTest %>/indexSpec.ts']
+				options: {
+					tsconfig: '<%= opt.client.tsTest %>/tsconfig.json'
+				}
 			}
 		},
 		tslint: {
